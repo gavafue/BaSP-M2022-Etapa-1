66354579c7e6c714f951fa945124dcca3ce1970c
Gabriel Vazquez <gavf.vazquez@gmail.com>
2022-04-15 22:14:57 -0300

    BaSP-W5-2022: ex05- for

---
+++ semana-05/exercises/05_for.js
@@ -0,0 +1,40 @@
+// For
+
+// 5a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando
+// un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
+var fiveWordsArray = ["one", "two", "three", "four", "five"];
+for (let i = 0; i < fiveWordsArray.length; i++) {
+    const element = fiveWordsArray[i];
+    alert(element);
+}
+
+// 5b) Al array anterior convertir la primera letra de cada palabra en mayúscula y
+// mostrar una alerta por cada palabra modificada.
+for (let i = 0; i < fiveWordsArray.length; i++) {
+    const element = fiveWordsArray[i];
+    const elementModified = element.substring(0, 1).toLocaleUpperCase() + element.substring(1);
+    alert(elementModified);
+
+}
+
+// 5c) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a)
+// recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence.
+// Al final mostrar una única alerta con la cadena completa.
+var sentence = "";
+for (let i = 0; i < fiveWordsArray.length; i++) {
+    const element = fiveWordsArray[i];
+    sentence += element;    
+}
+alert(sentence);
+
+
+// 5d) Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número 
+//de la repetición, es decir que al final de la ejecución del bucle for debería haber 10 elementos
+// dentro del array, desde el número 0 hasta al número 9. Mostrar por la consola del navegador el array
+// final (utilizar console.log).
+
+var tenNumbers = [];
+for (let i = 0; i < 10; i++) {
+   tenNumbers.push(i);
+}
+console.log(tenNumbers)
