049530b5a8466980c19d0985d2f0549be004286d
Gabriel Vazquez <gavf.vazquez@gmail.com>
2022-04-24 17:21:58 -0300

    BaSP-W6-2022: the first block of the validations of sign up were done

--- semana-06/javascript/log-in.js
+++ semana-06/javascript/log-in.js
@@ -12,20 +12,14 @@ function validationTextNumber(string) {
     }
     return hasLetter && hasNumber;
 }
-
 function validationEmail(email) {
     var emailConditions = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
     return emailConditions.test(email);
 }
-
-
-
-
 document.addEventListener('DOMContentLoaded', function () {
     //Function thas redirect the buttons with class "home" to the landing.
     var passwordInput = document.getElementById('password');
     var emailInput = document.getElementById('email');
-
     var homeButton = document.getElementsByClassName('home');
     for (var i = 0; i < homeButton.length; i++) {
         const boton = homeButton[i];
@@ -33,7 +27,6 @@ document.addEventListener('DOMContentLoaded', function () {
             window.location = 'index.html';
         })
     }
-
     var signUp = document.getElementsByClassName('sign-up');
     for (var i = 0; i < signUp.length; i++) {
         const boton = signUp[i];
@@ -41,7 +34,6 @@ document.addEventListener('DOMContentLoaded', function () {
             window.location = 'sign-up.html'
         })
     }
-
     passwordInput.addEventListener('blur', function () {
         var passwordValue = document.getElementById('password').value;
         if (validationTextNumber(passwordValue) == false) {
@@ -50,13 +42,11 @@ document.addEventListener('DOMContentLoaded', function () {
             passwordInput.classList = 'input-invalid';
         }
     })
-
     passwordInput.addEventListener('focus', function () {
         const passwordFeedback = document.getElementById('password-feedback');
         passwordFeedback.classList = 'feedback';
         passwordInput.classList = '';
     })
-
     emailInput.addEventListener('blur',function(){
         console.log(validationEmail(emailInput.value));
         if (validationEmail(emailInput.value) == false) {
@@ -65,7 +55,6 @@ document.addEventListener('DOMContentLoaded', function () {
             emailInput.classList = 'input-invalid';
         }
     })
-
     emailInput.addEventListener('focus', function () {
         const emailFeedback = document.getElementById('email-feedback');
         emailFeedback.classList = 'feedback';
--- semana-06/javascript/sign-up.js
+++ semana-06/javascript/sign-up.js
@@ -1,5 +1,4 @@
-function validationTextNumber(string) {
-    var hasNumber = false;
+function validationText(string) {
     var hasLetter = false;
     for (var i = 0; i < string.length; i++) {
         const element = string[i];
@@ -10,7 +9,18 @@ function validationTextNumber(string) {
             hasNumber = true;
         }
     }
-    return hasLetter && hasNumber;
+    return hasLetter;
+}
+
+function validationNumber(string) {
+    var hasNumber = false;
+    for (var i = 0; i < string.length; i++) {
+        const element = string[i];
+        if (!isNaN(element)) {
+            hasNumber = true;
+        }
+    }
+    return hasNumber;
 }
 
 function validationEmail(email) {
@@ -49,4 +59,80 @@ document.addEventListener('DOMContentLoaded', function () {
             window.location = 'log-in.html'
         })
     }
+    //validation from name and surname.
+    const signUpName = document.getElementById('register-name');
+    const signUpSurname = document.getElementById('register-surname');
+    signUpName.addEventListener('blur', function () {
+        if (!(validationText(signUpName.value) && validationStringLength(signUpName.value, 3))) {
+            const nameFeedback = document.getElementById('name-feedback');
+            nameFeedback.classList = 'feedback-invalid';
+            signUpName.classList = 'input-invalid';
+        }
+    })
+    signUpName.addEventListener('focus', function () {
+        const nameFeedback = document.getElementById('name-feedback');
+        nameFeedback.classList = 'feedback';
+        signUpName.classList = '';
+    })
+
+    signUpSurname.addEventListener('blur', function () {
+        if (!(validationText(signUpSurname.value) && validationStringLength(signUpSurname.value, 3))) {
+            const nameFeedback = document.getElementById('surname-feedback');
+            nameFeedback.classList = 'feedback-invalid';
+            signUpSurname.classList = 'input-invalid';
+        }
+    })
+    signUpSurname.addEventListener('focus', function () {
+        const nameFeedback = document.getElementById('surname-feedback');
+        nameFeedback.classList = 'feedback';
+        signUpSurname.classList = '';
+    })
+    //validation from email
+    const signUpEmail = document.getElementById('register-email');
+    signUpEmail.addEventListener('blur', function () {
+        if (!validationEmail(signUpEmail.value)) {
+            const emailFeedback = document.getElementById('email-feedback');
+            emailFeedback.classList = 'feedback-invalid';
+            signUpEmail.classList = 'input-invalid';
+        }
+    })
+    signUpEmail.addEventListener('focus', function () {
+        const emailFeedback = document.getElementById('email-feedback');
+        emailFeedback.classList = 'feedback';
+        signUpEmail.classList = '';
+    })
+
+    //validation from password
+    const passwordInput = document.getElementById('register-password');
+    passwordInput.addEventListener('blur', function () {
+        if (!validationText(passwordInput.value) || !validationNumber(passwordInput.value) 
+        || !validationStringLength(passwordInput.value, 8)) {
+            
+                const passwordFeedback = document.getElementById('password-feedback');
+                passwordFeedback.classList = 'feedback-invalid';
+                passwordInput.classList = 'input-invalid';
+            
+        }
+    })
+    passwordInput.addEventListener('focus', function () {
+        const passwordFeedback = document.getElementById('password-feedback');
+        passwordFeedback.classList = 'feedback';
+        passwordInput.classList = '';
+    })
+    // validation from repeat password
+    const repeatPasswordInput = document.getElementById('register-repeat-password');
+    repeatPasswordInput.addEventListener('blur', function () {
+        if (!validationText(repeatPasswordInput.value) || !validationNumber(repeatPasswordInput.value) ||
+            !validationStringLength(repeatPasswordInput.value, 8)) {
+            const passwordFeedback = document.getElementById('repeat-password-feedback');
+            passwordFeedback.classList = 'feedback-invalid';
+            repeatPasswordInput.classList = 'input-invalid';
+
+        }
+    })
+    repeatPasswordInput.addEventListener('focus', function () {
+        const passwordFeedback = document.getElementById('repeat-password-feedback');
+        passwordFeedback.classList = 'feedback';
+        repeatPasswordInput.classList = '';
+    })
 })
